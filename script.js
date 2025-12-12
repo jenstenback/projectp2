e631da2bc3e2424187b248e300cd2fddd441f120
jenstenback <344407@student.mboutrecht.nl>
2025-12-12 13:26:54 +0100

    script

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Kleine animatie bij het laden
 window.addEventListener("load", () => {
     document.body.classList.add("loaded");
 });
 
-// Voorbeeld interactie: klik op poster voor effect
 const poster = document.querySelector('.poster');
 if (poster) {
     poster.addEventListener('click', () => {
